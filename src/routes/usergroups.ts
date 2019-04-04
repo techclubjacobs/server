85ce5c7deb4e2b32480d4946c4811be3336c0a0c
Steven Abreu <stevenabreu7@gmail.com>
2019-04-04 17:09:08 +0200

    no explicit use of any

--- src/routes/usergroups.ts
+++ src/routes/usergroups.ts
@@ -1,11 +1,11 @@
-import * as express from "express";
 import * as db from '../tools/db';
+import { Application, Request, Response } from 'express';
 
-export const register = (app: express.Application) => {
+export const register = (app: Application) => {
     /* /usergroup endpoint */
     
     // Get method
-    app.get('/api/v1/usergroup', (req: any, res) => {
+    app.get('/api/v1/usergroup', (req: Request, res: Response) => {
         db.connection.query('SELECT * FROM UserGroup', (error, results, fields) => {
             if (error) {
                 return res.sendStatus(500);
@@ -17,7 +17,7 @@ export const register = (app: express.Application) => {
     });
 
     // Post method
-    app.post('/api/v1/usergroup', (req: any, res) => {
+    app.post('/api/v1/usergroup', (req: Request, res: Response) => {
         let title = req.query.title;
         let descrip = req.query.descrip;
 
@@ -41,7 +41,7 @@ export const register = (app: express.Application) => {
     /* /usergroup/:ugid */
 
     // Get method (Get specific UserGroup)
-    app.get('/api/v1/usergroup/:ugid', (req: any, res) => {
+    app.get('/api/v1/usergroup/:ugid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         if (!ugid) {
             return res.sendStatus(400);
@@ -60,7 +60,7 @@ export const register = (app: express.Application) => {
     });
 
     // PUT Method (Update Existing UserGroup)
-    app.put('/api/v1/usergroup/:ugid', (req: any, res) => {
+    app.put('/api/v1/usergroup/:ugid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let title = req.query.title;
         let descrip = req.query.descrip;
@@ -84,7 +84,7 @@ export const register = (app: express.Application) => {
     });
     
     // Delete Method (Remove user group)
-    app.delete('/api/v1/usergroup/:ugid', (req: any, res) => {
+    app.delete('/api/v1/usergroup/:ugid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         if (!ugid) {
             return res.sendStatus(400);
@@ -105,7 +105,7 @@ export const register = (app: express.Application) => {
     /* /usergroup/:ugid/members */
 
     // Get method (get all members of usergroup)
-    app.get('/api/v1/usergroup/:ugid/members', (req: any, res) => {
+    app.get('/api/v1/usergroup/:ugid/members', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         if (!ugid) {
             return res.sendStatus(400);
@@ -125,7 +125,7 @@ export const register = (app: express.Application) => {
     });
 
     // Post method (add new member to the usergroup)
-    app.post('/api/v1/usergroup/:ugid/members', (req: any, res) => {
+    app.post('/api/v1/usergroup/:ugid/members', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let user_id = req.query.user_id;
 
@@ -146,7 +146,7 @@ export const register = (app: express.Application) => {
     });
 
     // Delete Method (remove member from an usergroup)
-    app.delete('/api/v1/usergroup/:ugid/members/:uid', (req: any, res) => {
+    app.delete('/api/v1/usergroup/:ugid/members/:uid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let user_id = req.params.uid;
 
@@ -171,7 +171,7 @@ export const register = (app: express.Application) => {
     /* /usergroup/:ugid/owners */
     
     // Get method (get all owners of an usergroup)
-    app.get('/api/v1/usergroup/:ugid/owners', (req: any, res) => {
+    app.get('/api/v1/usergroup/:ugid/owners', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         if (!ugid) {
             return res.sendStatus(400);
@@ -191,7 +191,7 @@ export const register = (app: express.Application) => {
     });
 
     // Post method (add new owner to the usergroup)
-    app.post('/api/v1/usergroup/:ugid/owners', (req: any, res) => {
+    app.post('/api/v1/usergroup/:ugid/owners', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let user_id = req.query.user_id;
 
@@ -212,7 +212,7 @@ export const register = (app: express.Application) => {
     });
 
     // Delete Method (remove owner from an usergroup)
-    app.delete('/api/v1/usergroup/:ugid/owners/:uid', (req: any, res) => {
+    app.delete('/api/v1/usergroup/:ugid/owners/:uid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let user_id = req.params.uid;
 
@@ -237,7 +237,7 @@ export const register = (app: express.Application) => {
     /* /usergroup/:ugid/invited */
 
     // Get method (get all invited users of an usergroup)
-    app.get('/api/v1/usergroup/:ugid/invited', (req: any, res) => {
+    app.get('/api/v1/usergroup/:ugid/invited', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
 
         if (!ugid) {
@@ -258,7 +258,7 @@ export const register = (app: express.Application) => {
     });
 
     // Post method (invite new user to an usergroup)
-    app.post('/api/v1/usergroup/:ugid/invited', (req: any, res) => {
+    app.post('/api/v1/usergroup/:ugid/invited', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let invite_id = req.query.invite_id;
 
@@ -279,7 +279,7 @@ export const register = (app: express.Application) => {
     });
 
     // Delete Method (remove invitation of user to usergroup)
-    app.delete('/api/v1/usergroup/:ugid/invited/:uid', (req: any, res) => {
+    app.delete('/api/v1/usergroup/:ugid/invited/:uid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let invite_id = req.params.uid;
 
@@ -304,7 +304,7 @@ export const register = (app: express.Application) => {
     /* /usergroup/:ugid/requested */
 
     // Get method (get all requested users of usergroup)
-    app.get('/api/v1/usergroup/:ugid/requested', (req: any, res) => {
+    app.get('/api/v1/usergroup/:ugid/requested', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         if (!ugid) {
             return res.sendStatus(400);
@@ -324,7 +324,7 @@ export const register = (app: express.Application) => {
     });
 
     // Post method (add a new request to join an usergroup)
-    app.post('/api/v1/usergroup/:ugid/requested', (req: any, res) => {
+    app.post('/api/v1/usergroup/:ugid/requested', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let request_id = req.query.request_id;
 
@@ -347,7 +347,7 @@ export const register = (app: express.Application) => {
     });
 
     // Delete method (remove request to join an usergroup)
-    app.delete('/api/v1/usergroup/:ugid/requested/:uid', (req: any, res) => {
+    app.delete('/api/v1/usergroup/:ugid/requested/:uid', (req: Request, res: Response) => {
         let ugid = req.params.ugid;
         let request_id = req.params.uid;
 
