245f83f21fb3804a3034ed0e4e1438f295bbcc0c
ashray-00 <adhikariashray@gmail.com>
2019-04-08 17:07:25 +0200

    GET for ProductCategories, EventCategories and Locations

--- src/routes/metadata.ts
+++ src/routes/metadata.ts
@@ -4,4 +4,34 @@ import * as db from '../tools/db';
 // for location, categories, etc
 
 export const register = (app: express.Application) => {
+
+    //GET ProductCategories
+    app.get('/api/v1/prodcats',(req, res) =>{
+        db.connection.query('SELECT * from ProductCategory;',(error,results)=>{
+            if (error){
+                return res.sendStatus(200);
+            }
+            return res.send(results);
+        });
+    });
+
+    //GET EventCategories
+    app.get('/api/v1/evcats',(req, res) => {
+        db.connection.query('SELECT * from EventCategory;',(error,results)=>{
+            if (error){
+                return res.sendStatus(200);
+            }
+            return res.send(results);
+        });
+    });
+
+    //GET Locations
+    app.get('/api/v1/locations',(req,res) =>{
+        db.connection.query('SELECT * from Location;' ,(error,results)=>{
+            if (error){
+                return res.sendStatus(200);
+            }
+            return res.send(results);
+        });
+    });
 };
