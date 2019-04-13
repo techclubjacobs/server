5ffb0d525fb845469af1f4c60997a54ec55fe87c
Steven Abreu <stevenabreu7@gmail.com>
2019-04-13 11:11:38 -0400

    fix minor error that can be very frustrating (404)

--- src/tools/db.ts
+++ src/tools/db.ts
@@ -1,13 +1,15 @@
 import mysql from 'mysql';
 
 export const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
     database: 'JacobsApp',
-    port: 3306
+    host: 'localhost',
+    password: 'root',
+    port: 3306,
+    user: 'root',
 });
 
-connection.connect(function(err) {
-    if (err) console.log(err);
+connection.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
 });
