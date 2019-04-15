4adc103965c06c2d5c4f960884d963bb2ba7e8ac
Steven Abreu <stevenabreu7@gmail.com>
2019-04-15 13:05:03 -0400

    small fix (my bad)

--- src/tools/db.ts
+++ src/tools/db.ts
@@ -3,7 +3,7 @@ import mysql from "mysql";
 export const connection = mysql.createConnection({
     database: "JacobsApp",
     host: "localhost",
-    multipleStatements: true
+    multipleStatements: true,
     password: "root",
     port: 3306,
     user: "root",
